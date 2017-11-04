82055715f90ca003a7dc363186a672c159b84ad2
Alabe Duarte <alabeduarte@gmail.com>
2017-11-04 21:57:13 +1100

    fix(tmux): hardcod horizontal split to 15%

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# open vim on the left
 tmux new-session -d 'vim'
-tmux split-window -h 'console'
-tmux split-window -h
+
+# horizontal split with 15%
+tmux split-window -h -p 15
+
 tmux new-window 'dev'
 tmux -2 attach-session -d
